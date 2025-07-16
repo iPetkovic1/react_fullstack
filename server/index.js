e6e9852a7d87665439393a8275886b2ea51928f2
iPETKOVIC <igr.petkovic@gmail.com>
2025-07-16 02:34:05 +0200

    not_happy baš

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-const express = require("express");
+import express from "express";
+import db from "./models/index.js";
+
 const app = express();
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server is running on port 3001");
     });
+});
---
+++ server/models/Posts.js
@@ -0,0 +1,18 @@
+MediaSourceHandle.export = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        postText: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },);
+
+    return Posts;
+}  
